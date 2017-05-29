d1045de491dd5c3e0167177a4707301d63f76376
Ubuntu <frappe@vmERPNext.f5rwbzhxoccujitssj0emafjzg.ax.internal.cloudapp.net>
2017-05-29 11:12:54 +0000

    add mastar data status

---
+++ charity/charity_management_system/doctype/education_status/education_status.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Accurate Systems and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Education Status', {
+	refresh: function(frm) {
+
+	}
+});
