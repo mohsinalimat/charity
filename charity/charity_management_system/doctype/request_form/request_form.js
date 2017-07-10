5bae8a7762c89a215cbe80901311def7faea639c
mhbu50 <mhbu50@gmail.com>
2017-07-10 18:25:12 +0300

    cd

--- charity/charity_management_system/doctype/request_form/request_form.js
+++ charity/charity_management_system/doctype/request_form/request_form.js
@@ -4,5 +4,20 @@
 frappe.ui.form.on('Request Form', {
 	refresh: function(frm) {
 
+	},
+	onload:function (frm) {
+		frm.add_custom_button(__('Coupon'),
+			function() {
+				frm.trigger("make_coupon");
+			}, __("Make"));
+
+			frm.add_custom_button(__('Cheque'),
+				function() {
+					frm.trigger("make_cheque");
+				}, __("Make"));
+	},make_coupon:function(frm){
+alert("make_coupon")
+	},make_cheque:function(frm){
+alert("make_cheque")
 	}
 });
