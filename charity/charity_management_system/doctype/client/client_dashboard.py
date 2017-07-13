004bac7f2d980909eef11f4eb9ea7d3e6391071f
mhbu50 <mhbu50@gmail.com>
2017-07-13 21:16:27 +0300

    add dashbord to clint

---
+++ charity/charity_management_system/doctype/client/client_dashboard.py
@@ -0,0 +1,14 @@
+from frappe import _
+
+def get_data():
+	return {
+
+		'heatmap_message': _('This is based on the Time Sheets created against this project'),
+		'fieldname': 'request_number',
+		'transactions': [
+			{
+				'label': _('Coupon & Cheuq'),
+				'items': ['Coupon']
+			}
+		]
+	}
