a1a5c0c7d1cdd7d0291513d7eb336ad5cf6b7425
Mohammed R Abukhamseen <mhbu50@gmail.com>
2017-08-17 13:53:57 +0300

    Update conf.js

--- charity/public/js/conf.js
+++ charity/public/js/conf.js
@@ -5,16 +5,9 @@ frappe.provide('erpnext');
 
 // add toolbar icon
 $(document).bind('toolbar_setup', function() {
-	// frappe.app.name = "Smarte";
 
-	// frappe.help_feedback_link = '<p><a class="text-muted" \
-	// 	href="https://smartehis.com#contact">Feedback</a></p>'
-
-	$('.navbar-home').html('<img class="erpnext-icon" src="'+
-			frappe.urllib.get_base_url()+'/assets/charity/images/sihat_logo.jpg" />');
+	//$('.navbar-home').html('<img class="erpnext-icon" src="'+
+		//	frappe.urllib.get_base_url()+'/assets/charity/images/sihat_logo.jpg" />');
 });
 
-// preferred modules for breadcrumbs
-// $.extend(frappe.breadcrumbs.preferred, {
-// 	"Customer": "OP"
-// });
+
