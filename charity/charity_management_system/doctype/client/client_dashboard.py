5d7695189e6070b5269c0888706f8a80e4d041f7
mhbu50 <mhbu50@gmail.com>
2017-09-20 16:45:38 +0300

    add dashbord to client

--- charity/charity_management_system/doctype/client/client_dashboard.py
+++ charity/charity_management_system/doctype/client/client_dashboard.py
@@ -1,14 +1,23 @@
-# from frappe import _
-#
-# def get_data():
-# 	return {
-#
-# 		'heatmap_message': _('This is based on the Time Sheets created against this project'),
-# 		'fieldname': 'request_number',
-# 		'transactions': [
-# 			{
-# 				'label': _('Coupon & Cheuq'),
-# 				'items': ['Coupon']
-# 			}
-# 		]
-# 	}
+from frappe import _
+
+
+def get_data():
+    return {
+
+        'fieldname': 'file_number',
+        'transactions': [
+            {
+                'label': _('Coupon & Cheuq'),
+                'items': {
+                    "type": "doctype",
+                    "name": "Coupon",
+                    "description": _("Coupon"),
+                },
+                {
+                    "type": "doctype",
+                    "name": "Cheque Specification",
+                    "description": _("Cheque Specification"),
+                }
+            }
+        ]
+    }
