cb3184e7ea9bea37f36ca835956cfb4ab1d9f636
mhbu50 <mhbu50@gmail.com>
2017-09-21 03:06:21 +0300

    fix dashbord

--- charity/charity_management_system/doctype/client/client_dashboard.py
+++ charity/charity_management_system/doctype/client/client_dashboard.py
@@ -2,22 +2,12 @@ from frappe import _
 
 
 def get_data():
-    return {
-
-        'fieldname': 'file_number',
-        'transactions': [
-            {
-                'label': _('Coupon & Cheuq'),
-                'items': {
-                    "type": "doctype",
-                    "name": "Coupon",
-                    "description": _("Coupon"),
-                },
-                {
-                    "type": "doctype",
-                    "name": "Cheque Specification",
-                    "description": _("Cheque Specification"),
-                }
-            }
-        ]
-    }
+return {
+    'fieldname': 'file_number',
+    'transactions': [
+        {
+            'label': _('Coupon & Cheuq'),
+            'items': ['Coupon', 'Cheque Specification']
+        }
+    ]
+}
