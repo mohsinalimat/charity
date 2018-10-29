5f0bd6d3f4621794e835e2c892a75ca0e88e941c
mhbu50 <mhbu50>
2018-10-29 16:43:07 +0300

    fix listview_settings Request Form

--- charity/public/js/request_form_list.js
+++ charity/public/js/request_form_list.js
@@ -1,11 +1,10 @@
-frappe.provide("charity.request_form_list");
-
-console.log("in charity.request_form_list");
-
-if (frappe.user.has_role('Researcher')) {
-  console.log("you are Researcher = ", frappe.session.user);
-  frappe.route_options = {
-    "Researcher table.email": frappe.session.user
-  };
-  frappe.set_route('List', 'Request Form');
-}
+frappe.listview_settings['Request Form'] = {
+    onload: function(me) {
+      console.log("in charity.request_form_list");
+      if (frappe.user.has_role('Researcher')) {
+        console.log("you are Researcher = ", frappe.session.user);
+        frappe.route_options = {
+          "Researcher table.email": frappe.session.user
+        };
+      }
+    }
