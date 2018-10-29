81b66b28e1e197bc9c3bc65c814e3324d6cff5bb
mhbu50 <mhbu50>
2018-10-29 16:46:17 +0300

    syntax error

--- charity/public/js/request_form_list.js
+++ charity/public/js/request_form_list.js
@@ -1,10 +1,11 @@
 frappe.listview_settings['Request Form'] = {
-    onload: function(me) {
-      console.log("in charity.request_form_list");
-      if (frappe.user.has_role('Researcher')) {
-        console.log("you are Researcher = ", frappe.session.user);
-        frappe.route_options = {
-          "Researcher table.email": frappe.session.user
-        };
-      }
+  onload: function(me) {
+    console.log("in charity.request_form_list");
+    if (frappe.user.has_role('Researcher')) {
+      console.log("you are Researcher = ", frappe.session.user);
+      frappe.route_options = {
+        "Researcher table.email": frappe.session.user
+      };
     }
+  }
+}
